adc925bfcf5fc729721d471e2fa203df8a3af24c
auban <auban@stag-agency.be>
2018-10-30 23:43:21 +0100

    Add build small fix

--- README.md
+++ README.md
@@ -16,6 +16,110 @@ npm install rc-consent
 ```
 
 
+<h4>Parameters</h4>
+- **formIds**       = array of ids for add listen event on form submit. Each input checkbox with name category set the consent status.
+- **clickSelector** = string selector for add listen event on click. This selector can be use on button click accept consent current status by add a cookie with a hash value of the current statuses.
+- **cookie**        = object contain the name, days, domain and path of the stored cookie.  
+- **defaultStatus** = object of the default status key is the category and value is boolean.
 
+
+<h4>Usage/Example Form</h4>
+
+```html
+<script type="text/javascript" src="dist/rc-consent.js"></script>
+<script type="text/javascript" id="rcc_consent_GA">
+    window.addEventListener("load", function() {
+        window.rcc.addProvider({
+            id: 'ga',
+            trackingId: 'UA-XXXXX-Y',
+            anonymizeIp: true,
+            load: function() {
+                if (typeof ga !== 'undefined') { return false; }
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            },
+            onInitialise: function (rcc, status) {
+                console.log('GA Consent init');
+
+                if (rcc.hasConsented(this.category)) {
+                    console.log('hasConsented: true');
+                }
+                else {
+                    console.log('hasConsented: false');
+                }
+            },
+            onAllow: function (rcc) {
+                console.log('GA consent allow:' + this.category);
+
+                this.load();
+                ga('create', this.trackingId, 'auto');
+                ga('set', 'anonymizeIp', this.anonymizeIp);
+                ga('send', 'event', 'Cookie Consent', 'Accepted');
+                ga('send', 'pageview');
+            },
+            onRevoke: function (rcc) {
+                console.log('GA consent revoke:' + this.category);
+
+                this.load();
+
+                ga('create', this.trackingId, {'storage': 'none'});
+                ga('send', 'event', 'Cookie Consent', 'Declined');
+            }
+        });
+    });
+</script>
+<script>
+    window.addEventListener("load", function() {
+        window.rcc.initialise({
+            formIds: ['consent_form'], 
+            clickSelector: '.rcc-consent-button', 
+            cookie: {name: 'consent'}, 
+            defaultStatus: {required: true}
+        });
+    });
+</script>
+```
+
+```JS
+//Simple example jQuery with modal foundation
+(function($, rcc) {
+    "use strict";
+
+    var $consentModal = $('#consentModal');
+    var $consentModalInstance = new Foundation.Reveal($consentModal);
+
+    //Check has consented
+    $(window).on("load", function() {
+
+        if (!rcc.hasConsented()) {
+            $consentModal.foundation('open');
+        }
+    });
+
+    //On Submit close modal
+    $('#consent_form').submit(function (e) {
+
+        $consentModal.foundation('close');
+
+        return false;
+    });
+
+    //Init form field on Modal Open
+    $consentModal.on('open.zf.reveal', function () {
+
+        var consent_status = rcc.getStatus();
+
+        $( 'input[name][type="checkbox"]' ).each(function( index ) {
+
+            $( this ).attr("checked", (consent_status[this.name] === true ? true : false));
+        });
+
+
+    });
+
+})(jQuery, rcc);
+```
 
 [Demo]: http://redcastor.github.io/rc-consent/
--- dist/rc-consent.js
+++ dist/rc-consent.js
@@ -1,92 +1,59 @@
-/**
- * RC Consent
- *
- * Inspared by https://github.com/insites/cookieconsent
- */
 (function(rcc) {
     "use strict";
-
-    //Prevent run twice
     if (rcc.hasInitialised) {
         return;
     }
-
     var utils = {
-        // https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
         escapeRegExp: function(str) {
-            return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+            return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
         },
         hasClass: function(element, selector) {
-            var s = ' ';
-            return (
-                element.nodeType === 1 &&
-                (s + element.className + s)
-                    .replace(/[\n\t]/g, s)
-                    .indexOf(s + selector + s) >= 0
-            );
+            var s = " ";
+            return element.nodeType === 1 && (s + element.className + s).replace(/[\n\t]/g, s).indexOf(s + selector + s) >= 0;
         },
         addClass: function(element, className) {
-            element.className += ' ' + className;
+            element.className += " " + className;
         },
         removeClass: function(element, className) {
-            var regex = new RegExp('\\b' + this.escapeRegExp(className) + '\\b');
-            element.className = element.className.replace(regex, '');
+            var regex = new RegExp("\\b" + this.escapeRegExp(className) + "\\b");
+            element.className = element.className.replace(regex, "");
         },
         interpolateString: function(str, callback) {
             var marker = /{{([a-z][a-z0-9\-_]*)}}/gi;
             return str.replace(marker, function(matches) {
-                return callback(arguments[1]) || '';
+                return callback(arguments[1]) || "";
             });
         },
         getCookie: function(name) {
-            var value = '; ' + document.cookie;
-            var parts = value.split('; ' + name + '=');
-
-            value = parts.length < 2 ? undefined : parts.pop().split(';').shift();
-
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            value = parts.length < 2 ? undefined : parts.pop().split(";").shift();
             try {
                 var val_obj = JSON.parse(decodeURIComponent(value));
-
                 value = val_obj;
-            }
-            catch (e) {}
-
+            } catch (e) {}
             return value;
         },
         setCookie: function(name, value, expiryDays, domain, path, secure) {
             var exdate = new Date();
             exdate.setDate(exdate.getDate() + (expiryDays || 365));
-
             if (this.isPlainObject(value)) {
                 value = JSON.stringify(value);
             }
-
             value = encodeURIComponent(value);
-
-            var cookie = [
-                name + '=' + value,
-                'expires=' + exdate.toUTCString(),
-                'path=' + (path || '/')
-            ];
-
+            var cookie = [ name + "=" + value, "expires=" + exdate.toUTCString(), "path=" + (path || "/") ];
             if (domain) {
-                cookie.push('domain=' + domain);
+                cookie.push("domain=" + domain);
             }
             if (secure) {
-                cookie.push('secure');
+                cookie.push("secure");
             }
-
-            document.cookie = cookie.join(';');
+            document.cookie = cookie.join(";");
         },
-        // only used for extending the initial options
         deepExtend: function(target, source) {
             for (var prop in source) {
                 if (source.hasOwnProperty(prop)) {
-                    if (
-                        prop in target &&
-                        this.isPlainObject(target[prop]) &&
-                        this.isPlainObject(source[prop])
-                    ) {
+                    if (prop in target && this.isPlainObject(target[prop]) && this.isPlainObject(source[prop])) {
                         this.deepExtend(target[prop], source[prop]);
                     } else {
                         target[prop] = source[prop];
@@ -95,7 +62,6 @@
             }
             return target;
         },
-        // only used for throttling the 'mousemove' event (used for animating the revoke button when `animateRevokable` is true)
         throttle: function(callback, limit) {
             var wait = false;
             return function() {
@@ -108,27 +74,20 @@
                 }
             };
         },
-        // only used for hashing json objects (used for hash status objects)
         hash: function(str) {
-            var hash = 0,
-                i,
-                chr,
-                len;
+            var hash = 0, i, chr, len;
             if (str.length === 0) {
                 return hash;
             }
-
             for (i = 0, len = str.length; i < len; ++i) {
-
                 chr = str.charCodeAt(i);
                 hash = (hash << 5) - hash + chr;
                 hash |= 0;
             }
-
-            return hash & 0xfffffff; //Always positive
+            return hash & 268435455;
         },
         normaliseHex: function(hex) {
-            if (hex[0] === '#') {
+            if (hex[0] === "#") {
                 hex = hex.substr(1);
             }
             if (hex.length === 3) {
@@ -136,352 +95,217 @@
             }
             return hex;
         },
-        // used to get text colors if not set
         getContrast: function(hex) {
             hex = this.normaliseHex(hex);
             var r = parseInt(hex.substr(0, 2), 16);
             var g = parseInt(hex.substr(2, 2), 16);
             var b = parseInt(hex.substr(4, 2), 16);
-            var yiq = (r * 299 + g * 587 + b * 114) / 1000;
-            return yiq >= 128 ? '#000' : '#fff';
+            var yiq = (r * 299 + g * 587 + b * 114) / 1e3;
+            return yiq >= 128 ? "#000" : "#fff";
         },
-        // used to change color on highlight
         getLuminance: function(hex) {
-            var num = parseInt(this.normaliseHex(hex), 16),
-                amt = 38,
-                R = (num >> 16) + amt,
-                B = ((num >> 8) & 0x00ff) + amt,
-                G = (num & 0x0000ff) + amt;
-            var newColour = (
-                0x1000000 +
-                (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-                (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
-                (G < 255 ? (G < 1 ? 0 : G) : 255)
-            )
-                .toString(16)
-                .slice(1);
-            return '#' + newColour;
+            var num = parseInt(this.normaliseHex(hex), 16), amt = 38, R = (num >> 16) + amt, B = (num >> 8 & 255) + amt, G = (num & 255) + amt;
+            var newColour = (16777216 + (R < 255 ? R < 1 ? 0 : R : 255) * 65536 + (B < 255 ? B < 1 ? 0 : B : 255) * 256 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
+            return "#" + newColour;
         },
         isMobile: function() {
-            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-                navigator.userAgent
-            );
+            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
         },
         isPlainObject: function(obj) {
-            // The code "typeof obj === 'object' && obj !== null" allows Array objects
-            return (
-                typeof obj === 'object' && obj !== null && obj.constructor === Object
-            );
+            return typeof obj === "object" && obj !== null && obj.constructor === Object;
         },
         traverseDOMPath: function(elem, className) {
             if (!elem || !elem.parentNode) {
                 return null;
             }
-
             if (util.hasClass(elem, className)) {
                 return elem;
             }
-
             return this.traverseDOMPath(elem.parentNode, className);
         }
     };
-
     var providers = [];
     var cacheStatus = {};
-
     var defaultProvider = {
         id: null,
         trackingId: null,
-        category: 'analytics',
+        category: "analytics",
         onInitialise: function(rcc, status) {},
         onAllow: function(rcc) {},
         onRevoke: function(rcc) {},
         onStatusChange: function(rcc, new_status, old_status) {}
     };
-
     var defaultOptions = {
         formId: null,
         cookie: {
-            name: 'rcc_consent',
-            domain: '',
-            path: '/',
+            name: "rcc_consent",
+            domain: "",
+            path: "/",
             days: 7
         },
-        categories: [
-            'analytics'
-        ]
+        categories: [ "analytics" ]
     };
-
-    /**
-     * Set Consent for event
-     *
-     * @param event
-     * @param args
-     */
-    var setConsent = function( event, args ) {
-
-        if (!args && event.type === 'submit') {
-
-            var el_input = event.target.querySelectorAll('input[name]');
-
+    var setConsent = function(event, args) {
+        if (!args && event.type === "submit") {
+            var el_input = event.target.querySelectorAll("input[name]");
             if (el_input.length) {
                 args = {};
-
                 for (var i = 0; i < el_input.length; i++) {
-
-                    if(el_input[i].type === 'checkbox' && rcc.options.categories.indexOf(el_input[i].name) >= 0) {
+                    if (el_input[i].type === "checkbox" && rcc.options.categories.indexOf(el_input[i].name) >= 0) {
                         args[el_input[i].name] = el_input[i].checked;
                     }
                 }
             }
-
         }
-
         rcc.setConsent(args);
     };
-
-
-    /**
-     * Merge default status target with source not extend target
-     * @param target
-     * @param source
-     * @returns {*}
-     */
-    function mergeDefaultStatus (target, source) {
-
+    function mergeDefaultStatus(target, source) {
         for (var prop in target) {
             if (target.hasOwnProperty(prop)) {
-
                 if (source[prop] === true || source[prop] === false) {
                     target[prop] = source[prop];
                 }
             }
         }
-
         return target;
     }
-
-
     rcc = {
         initialise: function(options) {
-
-            //Init options
-            utils.deepExtend((this.options = {}), defaultOptions);
-
+            utils.deepExtend(this.options = {}, defaultOptions);
             if (utils.isPlainObject(options)) {
                 utils.deepExtend(this.options, options);
             }
-
-            //Add Event Listner for submit form ids
             if (this.options.formIds) {
-
                 for (var i_el_form = 0; i_el_form < this.options.formIds.length; i_el_form++) {
-
                     var el_form = window.document.getElementById(this.options.formIds[i_el_form]);
-
-                    el_form.addEventListener('submit', setConsent);
+                    el_form.addEventListener("submit", setConsent);
                 }
             }
-
-            //Add Event Listner for click button
             if (this.options.clickSelector) {
-
                 var el_click = window.document.querySelectorAll(this.options.clickSelector);
-
                 for (var i_el_click = 0; i_el_click < el_click.length; i_el_click++) {
-
-                    el_click[i_el_click].addEventListener('click', setConsent);
+                    el_click[i_el_click].addEventListener("click", setConsent);
                 }
             }
-
-            //Add Event Listner for set consent
-            window.document.addEventListener('rccSetConsent', setConsent);
-
-            //Init Categories
+            window.document.addEventListener("rccSetConsent", setConsent);
             var categories = [];
             var category;
-
             if (utils.isPlainObject(this.options.defaultStatus)) {
                 categories = Object.keys(this.options.defaultStatus);
             }
-
             for (var i_cat = 0; i_cat < providers.length; i_cat++) {
-                category = providers[i_cat].category || 'analytics';
-
+                category = providers[i_cat].category || "analytics";
                 if (categories.indexOf(category) === -1) {
                     categories.push(category);
                 }
-
-                //Add categories from provider
                 if (this.options.categories.indexOf(category) >= 0) {
                     this.options.categories.splice(this.options.categories.indexOf(category), 1);
                 }
             }
-
-            //Concat default
             this.options.categories = categories.concat(this.options.categories);
-
             var status = this.getStatus();
-
-
-            //Initialize each provider
             for (var i = 0; i < providers.length; i++) {
-
                 category = providers[i].category;
-
                 providers[i].onInitialise.call(providers[i], this, status);
-
                 if (category && status[category] === true) {
                     providers[i].onAllow.call(providers[i], this);
                 }
             }
-
             cacheStatus = status;
         },
-        addProvider: function (options) {
-
-            if (!options.id || options.id === '') {
+        addProvider: function(options) {
+            if (!options.id || options.id === "") {
                 return false;
             }
-
             var provider = {};
-
             utils.deepExtend(provider, defaultProvider);
-
             if (utils.isPlainObject(options)) {
                 utils.deepExtend(provider, options);
             }
-
             providers.push(provider);
-
             return true;
         },
         getProviders: function() {
             return providers;
         },
-        setConsent: function( args  ) {
-
+        setConsent: function(args) {
             this.setStatus(args);
-
             var status = this.getStatus();
             var is_status_change = false;
-
-            //Status change
             if (JSON.stringify(status) !== JSON.stringify(cacheStatus)) {
                 is_status_change = true;
             }
-
-            //Call onChange revoke or allow for each provider
             for (var i = 0; i < providers.length; i++) {
                 var category = providers[i].category;
-
                 if (is_status_change) {
                     providers[i].onStatusChange.call(providers[i], this, status, cacheStatus);
                 }
-
                 if (status[category] !== cacheStatus[category]) {
-
                     if (!status[category]) {
                         providers[i].onRevoke.call(providers[i], this);
-                    }
-                    else {
+                    } else {
                         providers[i].onAllow.call(providers[i], this);
                     }
                 }
             }
-
             cacheStatus = status;
         },
         getStatus: function() {
-
             var cookie = rcc.options.cookie;
             var curent_value = utils.getCookie(cookie.name);
-            var value_hash = utils.getCookie(cookie.name + '_hash');
+            var value_hash = utils.getCookie(cookie.name + "_hash");
             var value = this.getDefaultStatus();
-
             if (utils.isPlainObject(curent_value)) {
-
                 mergeDefaultStatus(value, curent_value);
             }
-
-            //Update cookie if current value not same as default value merge with current value.
             if (JSON.stringify(curent_value) !== JSON.stringify(value)) {
-
                 utils.setCookie(cookie.name, value, 0, cookie.domain, cookie.path);
             }
-
-            //Remove hash not same as cookie status
             if (value_hash !== utils.hash(JSON.stringify(value))) {
-
-                utils.setCookie(this.options.cookie.name + '_hash', '', -1, cookie.domain, cookie.path);
+                utils.setCookie(this.options.cookie.name + "_hash", "", -1, cookie.domain, cookie.path);
             }
-
             return value;
         },
-        setStatus: function( value ) {
-
+        setStatus: function(value) {
             var cookie = this.options.cookie;
-
             if (!utils.isPlainObject(value)) {
-
                 var curent_value = utils.getCookie(cookie.name);
-
                 value = this.getDefaultStatus();
-
                 if (utils.isPlainObject(curent_value)) {
                     mergeDefaultStatus(value, curent_value);
                 }
-
             }
-
             var cookie_hash = utils.hash(JSON.stringify(value));
-
             utils.setCookie(cookie.name, value, 0, cookie.domain, cookie.path);
-            utils.setCookie(cookie.name + '_hash', cookie_hash, cookie.days, cookie.domain, cookie.path);
+            utils.setCookie(cookie.name + "_hash", cookie_hash, cookie.days, cookie.domain, cookie.path);
         },
         clearStatus: function() {
-
             var cookie = this.options.cookie;
-
-            utils.setCookie(cookie.name, '', -1, cookie.domain, cookie.path);
-            utils.setCookie(cookie.name + '_hash', '', -1, cookie.domain, cookie.path);
-
+            utils.setCookie(cookie.name, "", -1, cookie.domain, cookie.path);
+            utils.setCookie(cookie.name + "_hash", "", -1, cookie.domain, cookie.path);
             cacheStatus = this.getDefaultStatus();
         },
         getDefaultStatus: function() {
-
             var default_value = utils.isPlainObject(this.options.defaultStatus) ? this.options.defaultStatus : {};
             var status = {};
-
             for (var i = 0; i < this.options.categories.length; i++) {
-
                 var cat_status = default_value[this.options.categories[i]];
-
                 status[this.options.categories[i]] = cat_status === true ? true : false;
             }
-
             return status;
         },
-        hasConsented: function ( category ) {
-
+        hasConsented: function(category) {
             var status = this.getStatus();
-
-            var value_hash = utils.getCookie(this.options.cookie.name + '_hash');
-
+            var value_hash = utils.getCookie(this.options.cookie.name + "_hash");
             if (!value_hash) {
                 return false;
-            }
-            else if (!category) {
+            } else if (!category) {
                 return true;
             }
-
             return !!status[category];
         }
     };
-
-
-    //Prevent run twice
     rcc.hasInitialised = true;
-
     window.rcc = rcc;
 })(window, window.rcc || {});
+//# sourceMappingURL=rc-consent.js.map
